6dd6a6b95b899edb15ad45ea4f58d9733e99caa9
a.kraynov <a.kraynov@astrio.net>
2024-07-08 21:13:40 +0400

    iter4: добавлены переменные командной строки

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -14,7 +14,7 @@ func InitConfig() (*Config, error) {
 	cfg := &Config{}
 
 	flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "Адрес HTTP-сервера")
-	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8000/", "Базовый адрес для сокращенных URL")
+	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "Базовый адрес для сокращенных URL")
 	flag.Parse()
 
 	if cfg.ServerAddress == "" {
