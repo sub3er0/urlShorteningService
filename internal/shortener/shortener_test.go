035402f7467ac3a52c26a456bb0e0db27e606ae5
a.kraynov <a.kraynov@astrio.net>
2024-07-23 12:56:26 +0400

    iter7: добавлены тесты

--- internal/shortener/shortener_test.go
+++ internal/shortener/shortener_test.go
@@ -116,3 +116,124 @@ func TestURLShortener_JsonPostHandlerExistedUrl(t *testing.T) {
 	body := w.Body.String()
 	assert.Contains(t, body, "\"result\":\"http://localhost:8080/shortURL\"", "Body must looks like \"result\":\"http://localhost\"")
 }
+
+func TestURLShortener_PostHandler(t *testing.T) {
+	us := &URLShortener{
+		Storage:       &storage.InMemoryStorage{Urls: make(map[string]string)},
+		ServerAddress: "localhost:8080",
+		BaseURL:       "http://localhost:8080/",
+	}
+
+	req, err := http.NewRequest(http.MethodPost, "http://localhost/", bytes.NewReader([]byte("https://www.example.com")))
+	assert.NoError(t, err)
+
+	w := httptest.NewRecorder()
+	us.PostHandler(w, req)
+	assert.Equal(t, http.StatusCreated, w.Code, "Invalid status code")
+
+	body := w.Body.String()
+	assert.Contains(t, body, "http://localhost", "Invalid url in response body")
+}
+
+func TestGetHandler_ValidRequest(t *testing.T) {
+	us := &URLShortener{
+		Storage: &storage.InMemoryStorage{
+			Urls: map[string]string{"shortURL": "https://www.example.com"},
+		},
+	}
+	_, err := http.NewRequest(http.MethodGet, "/shortURL", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	originalURL, ok := us.Storage.GetURL("shortURL")
+
+	if ok {
+		w.Header().Set("Location", originalURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	}
+
+	assert.Equal(t, http.StatusTemporaryRedirect, w.Code, "Invalid status code")
+
+	location := w.Header().Get("Location")
+	assert.Equal(t, "https://www.example.com", location, "Invalid Location header value")
+}
+
+func TestPostHandler_InvalidMethod(t *testing.T) {
+	us := &URLShortener{
+		Storage: &storage.InMemoryStorage{
+			Urls: map[string]string{"shortURL": "https://www.example.com"},
+		},
+		ServerAddress: "localhost:8080",
+		BaseURL:       "http://localhost:8080/",
+	}
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	us.PostHandler(w, req)
+	assert.Equal(t, http.StatusBadRequest, w.Code, "Invalid status code")
+
+	body := w.Body.String()
+	assert.Equal(t, "Only POST requests are allowed!\n", body, "Invalid response message")
+}
+
+func TestPostHandler_InvalidURL(t *testing.T) {
+	us := &URLShortener{
+		Storage: &storage.InMemoryStorage{
+			Urls: map[string]string{},
+		},
+		ServerAddress: "localhost:8080",
+		BaseURL:       "http://localhost:8080/",
+	}
+	body := []byte("invalid-url")
+	req, err := http.NewRequest(http.MethodPost, "/", bytes.NewReader(body))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	us.PostHandler(w, req)
+	assert.Equal(t, http.StatusBadRequest, w.Code, "Invalid URL")
+}
+
+func TestURLShortener_PostHandlerExistedUrl(t *testing.T) {
+	us := &URLShortener{
+		Storage: &storage.InMemoryStorage{
+			Urls: map[string]string{"shortURL": "https://www.example.com"},
+		},
+		ServerAddress: "localhost:8080",
+		BaseURL:       "http://localhost:8080/",
+	}
+
+	req, err := http.NewRequest(http.MethodPost, "http://localhost/", bytes.NewReader([]byte("https://www.example.com")))
+	assert.NoError(t, err)
+
+	w := httptest.NewRecorder()
+	us.PostHandler(w, req)
+	assert.Equal(t, http.StatusCreated, w.Code, "Incorrect status code")
+
+	body := w.Body.String()
+	assert.Contains(t, body, "http://localhost:8080/shortURL", "Body must looks like http://localhost:8080/shortURL")
+}
+
+func TestURLShortener_GetShortKeyExist(t *testing.T) {
+	us := &URLShortener{
+		Storage: &storage.InMemoryStorage{
+			Urls: map[string]string{"shortURL": "https://www.example.com"},
+		},
+		ServerAddress: "localhost:8080",
+		BaseURL:       "http://localhost:8080/",
+	}
+	shortKey, ok := us.getShortURL("shortURL")
+
+	if ok {
+		assert.Equal(t, "shortURL", shortKey, "Incorrect Short URL")
+	}
+}
