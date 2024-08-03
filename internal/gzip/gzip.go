5c7c70c359693fc589c78a531137235e978c7056
a.kraynov <a.kraynov@astrio.net>
2024-08-03 19:56:22 +0400

    iter9: исправление замечание к пр

--- internal/gzip/gzip.go
+++ internal/gzip/gzip.go
@@ -61,19 +61,33 @@ func contains(target string, list []string) bool {
 
 func GzipRequestDecompressor(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rw := w
+
+		// Проверка на сжатие ответа
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			gz := gzip.NewWriter(w)
+			defer func(gz *gzip.Writer) {
+				err := gz.Close()
+				if err != nil {
+					log.Fatalf("Error closing gzip.Writer: %s", err)
+				}
+			}(gz)
+
+			rw = &gzipResponseWriter{w: w, gz: gz}
+			rw.Header().Set("Content-Encoding", "gzip")
+		}
+
+		// Распаковка запроса
 		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
 			reader, err := gzip.NewReader(r.Body)
-
 			if err != nil {
-				http.Error(w, "Error decompressing request body", http.StatusInternalServerError)
+				http.Error(rw, "Error decompressing request body", http.StatusInternalServerError)
 				return
 			}
-
 			defer reader.Close()
 			r.Body = io.NopCloser(reader)
-			next.ServeHTTP(w, r)
-		} else {
-			next.ServeHTTP(w, r)
 		}
+
+		next.ServeHTTP(rw, r)
 	})
 }
--- internal/logger/server_logger.go
+++ internal/logger/server_logger.go
@@ -1,37 +1,36 @@
 package logger
 
 import (
+	"fmt"
 	"github.com/go-chi/chi/v5/middleware"
 	"go.uber.org/zap"
-	"log"
 	"net/http"
 	"time"
 )
 
 var Sugar zap.SugaredLogger
 
-func ResponseLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		rw := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
-		defer func() {
-			log.Printf("Response: Status=%d, ContentLength=%d", rw.Status(), rw.BytesWritten())
-		}()
-
-		next.ServeHTTP(rw, r)
-	})
-}
-
 func RequestLogger(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		uri := r.RequestURI
 		method := r.Method
-		h.ServeHTTP(w, r)
-		duration := time.Since(start)
-		Sugar.Infoln(
-			"uri", uri,
-			"method", method,
-			"duration", duration,
-		)
+		rw := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+		defer func() {
+			Sugar.Infoln(
+				fmt.Sprintf("Response: Status=%d", rw.Status()),
+				fmt.Sprintf("ContentLength=%d", rw.BytesWritten()),
+			)
+
+			duration := time.Since(start)
+			Sugar.Infoln(
+				"uri", uri,
+				"method", method,
+				"duration", duration,
+			)
+		}()
+
+		h.ServeHTTP(rw, r)
 	})
 }
