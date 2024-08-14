20df8d5080be4b75ffd419b96660fca9f12abc3c
a.kraynov <a.kraynov@astrio.net>
2024-08-15 00:39:17 +0400

    iter12: проверка url на корректность

--- internal/shortener/shortener.go
+++ internal/shortener/shortener.go
@@ -55,7 +55,6 @@ func (us *URLShortener) GetHandler(w http.ResponseWriter, r *http.Request) {
 
 		if err != nil || (parsedURL.Scheme != "http" && parsedURL.Scheme != "https") {
 			w.Header().Set("Location", storedURL)
-			w.WriteHeader(http.StatusOK)
 			http.Error(w, "HTTP redirect blocked", http.StatusNotFound)
 			return
 		}
